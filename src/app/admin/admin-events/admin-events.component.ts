8a50db9c96bab9ed4365046f2e7ae1890a8f1e76
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2020-05-28 22:25:54 +0200

    finish projects and responsive design

--- src/app/admin/admin-events/admin-events.component.ts
+++ src/app/admin/admin-events/admin-events.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Event } from './events.model';
+import { EventService } from './events.service';
 @Component({
   selector: 'app-admin-events',
   templateUrl: './admin-events.component.html',
   styleUrls : ['./admin-events.component.css']
 })
-export class AdminEventsComponent {
+export class AdminEventsComponent implements OnInit {
+  events: Event[] = [];
+  private eventsSub: Subscription;
+  isLoading = false;
+  totalEvents = 0;
+  postsPerPage = 10;
+  currentPage = 1;
+  pageSizeOptions = [1, 2 , 5, 10];
+  constructor(public eventsService: EventService) {}
 
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.eventsService.getEvent(this.postsPerPage, this.currentPage);
+    this.eventsSub = this.eventsService.getEventUpdateListener()
+      .subscribe(
+        (eventData:{ events : Event[] ,postCount : number } ) => {
+          this.isLoading = false;
+          this.totalEvents = eventData.postCount;
+          this.events = eventData.events;
+        });
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.eventsSub.unsubscribe();
+  }
 }
