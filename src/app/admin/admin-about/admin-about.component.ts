cc136e7940cd331396e186f8ec4752db38286e80
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2020-05-31 02:39:14 +0200

    finish total number in admin

--- src/app/admin/admin-about/admin-about.component.ts
+++ src/app/admin/admin-about/admin-about.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { AboutText } from './aboutText.modal';
+import { AboutTextService } from './adminText.service';
 @Component({
   selector: 'app-admin-about',
   templateUrl: './admin-about.component.html',
   styleUrls : ['./admin-about.component.scss']
 })
-export class AdminAboutComponent {
+export class AdminAboutComponent  implements OnInit , OnDestroy {
+  aboutText: AboutText[] = [];
+  private aboutTextSub: Subscription;
+  isLoading = false;
+  totalAboutText = 0;
+  postsPerPage = 10;
+  currentPage = 1;
+  pageSizeOptions = [1, 2 , 5, 10];
+  constructor(public aboutTextService: AboutTextService) {}
 
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.aboutTextService.getAboutText(this.postsPerPage, this.currentPage);
+    this.aboutTextSub = this.aboutTextService.getAboutTextUpdateListener()
+      .subscribe(
+        (aboutTextData:{ AboutText : AboutText[] ,postCount : number } ) => {
+          this.isLoading = false;
+          this.totalAboutText = aboutTextData.postCount;
+          this.aboutText = aboutTextData.AboutText;
+        });
+  }
  
+
+  ngOnDestroy() {
+    this.aboutTextSub.unsubscribe();
+  }
+
 }
--- src/app/admin/admin-partners/admin-partners.component.ts
+++ src/app/admin/admin-partners/admin-partners.component.ts
@@ -1,13 +1,39 @@
-import { Component , OnInit } from '@angular/core';
+import { Component , OnInit, OnDestroy } from '@angular/core';
+import { Partner } from './partner.modal';
+import { Subscription } from 'rxjs';
+import { PartnerService } from './partners.service';
+import { NotificationService } from '../header-admin/notifications.service';
 @Component ({
   selector : 'app-partners-admin',
   templateUrl : './admin-partners.component.html',
   styleUrls : ['./admin-partners.component.scss']
 })
 
-export class AdminPartnersComponent implements OnInit {
-  constructor() {
-   }
+export class AdminPartnersComponent implements OnInit, OnDestroy {
+  partners: Partner[] = [];
+  private partnersSub: Subscription;
+  isLoading = false;
+  totalPartners = 0;
+  postsPerPage = 10;
+  currentPage = 1;
+  pageSizeOptions = [1, 2 , 5, 10];
+  constructor(public partnersService: PartnerService,
+    public notificationService: NotificationService) {}
+  
+
   ngOnInit() {
+    this.isLoading = true;
+    this.partnersService.getPartner(this.postsPerPage, this.currentPage);
+    this.partnersSub = this.partnersService.getPartnerUpdateListener()
+      .subscribe(
+        (partnerData:{ partners : Partner[] ,postCount : number } ) => {
+          this.isLoading = false;
+          this.totalPartners = partnerData.postCount;
+          this.partners = partnerData.partners;
+        });
+  }
+
+  ngOnDestroy() {
+    this.partnersSub.unsubscribe();
   }
 }
