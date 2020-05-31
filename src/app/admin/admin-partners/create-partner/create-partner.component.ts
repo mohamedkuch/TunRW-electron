7942918dfd6582cc3ea3581ed31cb79d1e1049bf
mohamedkush <mohamed.kechaou@alumni.fh-aachen.de>
2020-05-31 02:08:14 +0200

    update partners

--- src/app/admin/admin-partners/create-partner/create-partner.component.ts
+++ src/app/admin/admin-partners/create-partner/create-partner.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { PartnerService } from '../partners.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Partner } from '../partner.modal';
 import { mimeType } from '../../mime-type.validator';
-@Component ({
-  selector : 'app-create-partner',
-  templateUrl : './create-partner.component.html',
-  styleUrls : ['./create-partner.component.scss']
+@Component({
+  selector: 'app-create-partner',
+  templateUrl: './create-partner.component.html',
+  styleUrls: ['./create-partner.component.scss']
 })
 
 export class CreatePartnerComponent implements OnInit {
@@ -19,19 +19,18 @@ export class CreatePartnerComponent implements OnInit {
   partner: Partner;
   isLoading = false;
   form: FormGroup;
-  imagePreview: any;
 
   constructor(public partnersService: PartnerService,
-              public route: ActivatedRoute) {}
+    public route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      title : new FormControl(null, {validators: [Validators.required , Validators.minLength(0)]}),
+      title: new FormControl(null, { validators: [Validators.required, Validators.minLength(0)] }),
       image: new FormControl(null, {
-        validators: [Validators.required ] ,
+        validators: [Validators.required],
         asyncValidators: [mimeType]
       }
-        )
+      )
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -41,28 +40,16 @@ export class CreatePartnerComponent implements OnInit {
         this.isLoading = true;
         this.partnersService.getSinglePartner(this.partnerId).subscribe(postData => {
           this.isLoading = false;
-          this.partner = {id: postData._id,  title: postData.title, imagePath: postData.imagePath , creator:postData.creator};
-          this.form.setValue({title: this.partner.title , image: this.partner.imagePath});
+          this.partner = { id: postData._id, title: postData.title, imagePath: postData.imagePath, creator: postData.creator };
+          this.form.setValue({ title: this.partner.title, image: this.partner.imagePath });
         });
       } else {
         this.mode = 'create';
         this.partnerId = null;
       }
     });
-    this.imagePreview = '';
   }
-  onImagePicked(event) {
-    const file = (event.target as HTMLInputElement).files[0];
-    this.form.patchValue({image: file});
-    this.form.get('image').updateValueAndValidity();
-    const reader = new FileReader();
-    reader.onload = () => {
-      this.imagePreview = reader.result;
-    };
-    reader.readAsDataURL(file);
-    this.imagePreview = this.imagePreview.toString();
-  
-  }
+
   onSavePartner() {
     if (this.form.invalid) {
       this.errorFlag = true;
@@ -70,16 +57,13 @@ export class CreatePartnerComponent implements OnInit {
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-
-      this.partnersService.addPartner( this.form.value.title,
+      this.partnersService.addPartner(this.form.value.title,
         this.form.value.image
-        );
-
-
+      );
     } else {
       this.partnersService.updatePartner(this.partnerId, this.form.value.title,
         this.form.value.image
-        );
+      );
     }
     this.errorFlag = false;
     this.form.reset();
--- src/app/admin/admin-partners/partners.service.ts
+++ src/app/admin/admin-partners/partners.service.ts
@@ -7,80 +7,72 @@ import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
 
 const BACKEND_URL = environment.apiUrl + "/partners";
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PartnerService {
   private partners: Partner[] = [];
-  private partnersUpdated = new Subject<{partners: Partner[] , postCount: number}>();
+  private partnersUpdated = new Subject<{ partners: Partner[], postCount: number }>();
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) { }
   // get All Partners
   getPartner(postsPerPage: number, currentPage: number) {
     const queryParams = `?pageSize=${postsPerPage}&page=${currentPage}`;
-    this.http.get<{message: string, partners: any, maxPosts: number}>(BACKEND_URL + queryParams)
+    this.http.get<{ message: string, partners: any, maxPosts: number }>(BACKEND_URL + queryParams)
       .pipe(map((data) => {
-          return {
-            partners : data.partners.map(post => {
-              return {
-                id : post._id,
-                title : post.title,
-                imagePath: post.imagePath,
-                creator: post.creator
-              };
-            }) ,
-            maxPosts : data.maxPosts
+        return {
+          partners: data.partners.map(post => {
+            return {
+              id: post._id,
+              title: post.title,
+              imagePath: post.imagePath,
+              creator: post.creator
+            };
+          }),
+          maxPosts: data.maxPosts
         };
       }))
       .subscribe((finalData) => {
-            console.log(finalData);
-            this.partners = finalData.partners;
-            this.partnersUpdated.next({partners: [...this.partners] , postCount : finalData.maxPosts});
+        console.log(finalData);
+        this.partners = finalData.partners;
+        this.partnersUpdated.next({ partners: [...this.partners], postCount: finalData.maxPosts });
       });
   }
 
   // get one Partner
-  getSinglePartner(id: string){
-    return this.http.get<{_id: string;
-        title: string;
-        imagePath: string;
-        creator: string
-      }>(BACKEND_URL + "/" + id);
+  getSinglePartner(id: string) {
+    return this.http.get<{
+      _id: string;
+      title: string;
+      imagePath: string;
+      creator: string
+    }>(BACKEND_URL + "/" + id);
   }
 
   // Add New Partner
-  addPartner(title: string,  image: File) {
+  addPartner(title: string, image: string) {
     const postData = new FormData();
     postData.append('title', title);
-    postData.append('image', image, title);
-    console.log("adding partner", postData);
-    this.http.post<{message: string, partner: Partner}>(BACKEND_URL, postData)
-    .subscribe((data) => {
-      this.router.navigate(['/admin/Partners']);
-    });
+    postData.append('imagePath', image);
+    this.http.post<{ message: string, partner: Partner }>(BACKEND_URL, postData)
+      .subscribe((data) => {
+        this.router.navigate(['/admin/Partners']);
+      });
 
   }
 
   // update Partner
-  updatePartner(id: string ,title: string,  image: File | string) {
-    let postData: Partner | FormData;
-    if(typeof(image) === 'object') {
-       postData = new FormData();
-      postData.append("title", title);
-      postData.append("id", id);
-      postData.append("image", image, title);
-
-    } else {
-       postData = {
-            id : id,
-            title: title,
-            imagePath: image,
-            creator: null
-      }
+  updatePartner(id: string, title: string, image: string) {
+    let postData: Partner;
+    postData = {
+      id: id,
+      title: title,
+      imagePath: image,
+      creator: null
     }
-    this.http.put(BACKEND_URL + "/" +  id, postData)
-    .subscribe((data) => {
-       const updatedPartners = [...this.partners];
-       const oldPartnerIndex = updatedPartners.findIndex(p => p.id === id);
-       this.router.navigate(['/admin/Partners']);
+    this.http.put(BACKEND_URL + "/" + id, postData)
+      .subscribe((data) => {
+        const updatedPartners = [...this.partners];
+        const oldPartnerIndex = updatedPartners.findIndex(p => p.id === id);
+        this.router.navigate(['/admin/Partners']);
       });
   }
 
